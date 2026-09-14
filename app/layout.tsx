d04a0fa7ae5316246e7e0cfa5d489e1ba6814bd4
Nara Tseng <nara@Mac.home>
2026-09-14 21:27:19 +0800

    ＰＩＸＥＬＯＧＩＣ.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { ThemeProvider } from '@/components/ThemeProvider';
 export const metadata: Metadata = {
-  title: 'Nara‘s Portfolio & Profile',
-  description: 'Nara‘s Portfolio & Profile',
+  title: 'ＰＩＸＥＬＯＧＩＣ.'
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
